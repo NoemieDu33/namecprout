f46b87e7bad5feb1dfef0f9e9b5543ae244281b5
Noémie <99049164+NoemieDu33@users.noreply.github.com>
2024-05-16 17:10:24 +0200

    Add files via upload

--- colorsensor.py
+++ colorsensor.py
@@ -1,58 +1,68 @@
 import RPi.GPIO as GPIO
 import time
 
-# définition des broches utilisées
-broche_S3 = 27  # S0 du TSC3200 à GPIO27 du Raspberry Pi
-broche_S2 = 22  # S1 du TSC3200 à GPIO22 du Raspberry Pi
-broche_S1 = 23  # S2 du TSC3200 à GPIO23 du Raspberry Pi
-broche_S0 = 24  # S3 du TSC3200 à GPIO24 du Raspberry Pi
-broche_OUT = 17 # OUT du TSC3200 à GPIO25 du Rasperry Pi
+s2 = 23
+s3 = 24
+signal = 25
+NUM_CYCLES = 10
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(broche_OUT,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(broche_S0,GPIO.OUT)
-GPIO.setup(broche_S1,GPIO.OUT)
-GPIO.setup(broche_S2,GPIO.OUT)
-GPIO.setup(broche_S3,GPIO.OUT)
 
-# réglage de la fréquence de sortie du capteur (2% du maximum)
-GPIO.output(broche_S0,GPIO.LOW)
-GPIO.output(broche_S1,GPIO.HIGH)
-
-def mesure():
-    time.sleep(0.1)
-    debut = time.time()
-    for impulse_count in range(10):
-        GPIO.wait_for_edge(broche_OUT, GPIO.FALLING)
-    return time.time() - debut      
-
-while(1):  
-
-    GPIO.output(broche_S2,GPIO.LOW)
-    GPIO.output(broche_S3,GPIO.LOW)
-    rouge = mesure()
-    print("Rouge: ",rouge)
-
-    GPIO.output(broche_S2,GPIO.LOW)
-    GPIO.output(broche_S3,GPIO.HIGH)
-    bleu = mesure()
-    print("Bleu: ",bleu)
-
-    GPIO.output(broche_S2,GPIO.HIGH)
-    GPIO.output(broche_S3,GPIO.HIGH)
-    vert = mesure()
-    print("Vert: ",vert)
-
-    GPIO.output(broche_S2,GPIO.HIGH)
-    GPIO.output(broche_S3,GPIO.LOW)
-    total = mesure()
-    print("Sans filtre: ",total)
-
-  # ici, vous pouvez ajouter des conditions afin de détecter
-  # un objet connu, par exemple:
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(s2,GPIO.OUT)
+  GPIO.setup(s3,GPIO.OUT)
+  print("\n")
   
-    if ((abs(rouge - 0.22) < 0.02) and (abs(bleu - 0.25) < 0.02) and (abs(vert - 0.32) < 0.02)):
-        print("C'est le carton noir!")
 
-    print("\n")
-    time.sleep(1)  
+
+
+
+def loop():
+  temp = 1
+  while(1):  
+
+    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s3,GPIO.LOW)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start      #seconds to run for loop
+    red  = NUM_CYCLES / duration   #in Hz
+    print("red value - ",red)
+
+    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s3,GPIO.HIGH)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    blue = NUM_CYCLES / duration
+    print("blue value - ",blue)
+
+    GPIO.output(s2,GPIO.HIGH)
+    GPIO.output(s3,GPIO.HIGH)
+    time.sleep(0.3)
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    green = NUM_CYCLES / duration
+    print("green value - ",green)
+    time.sleep(2)  
+
+
+def endprogram():
+    GPIO.cleanup()
+
+if __name__=='__main__':
+    
+    setup()
+
+    try:
+        loop()
+
+    except KeyboardInterrupt:
+        endprogram()
