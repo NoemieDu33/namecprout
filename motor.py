3912f7c1ff0744a506531132b9f16ca8d544d42d
NoemieDu33 <hatenasp2@gmail.com>
2024-05-24 18:54:44 +0200

    ptn enft c du pwm

--- motor.py
+++ motor.py
@@ -7,20 +7,24 @@ class Motor:
         self.step_pin = 13 #13
         self.dir_pin = 17   #1
 
-        gpio.setup(self.step_pin,gpio.OUT)
+        #gpio.setup(self.step_pin,gpio.OUT)
+        self.pwm = gpio.PWM(self.step_pin, 0.5)
         gpio.setup(self.dir_pin,gpio.OUT)
 
     def one_full_turn(self):
         delay = 0.0005 #500microsecods
         gpio.output(self.dir_pin, gpio.HIGH)
         for x in range(200):
-            gpio.output(self.step_pin, gpio.HIGH)
+            self.pwm.start(100)
+            #gpio.output(self.step_pin, gpio.HIGH)
             #print("High signal step")
             time.sleep(delay)
-            gpio.output(self.step_pin, gpio.LOW)
+            self.pwm.stop()
+            #gpio.output(self.step_pin, gpio.LOW)
             #print("Low signal step")
             time.sleep(delay)
         gpio.output([self.dir_pin, self.step_pin], gpio.LOW)
+        gpio.cleanup()
 
 
 if __name__=="__main__":
