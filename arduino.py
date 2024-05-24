eadc3ce181b714b0ed87452ddd96e0545a8ac155
NoemieDu33 <hatenasp2@gmail.com>
2024-05-25 01:08:57 +0200

    cest parti pour serial

---
+++ arduino.py
@@ -0,0 +1,9 @@
+import serial,time
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        time.sleep(1)
