f276902bb7e285dcd2a7e6c4e6b2180b3b57e9c8
NoemieDu33 <hatenasp2@gmail.com>
2024-05-24 18:43:51 +0200

    motor banger

--- motor.py
+++ motor.py
@@ -1,2 +1,26 @@
+import RPi.GPIO as gpio
+import time
+
 class Motor:
-    pass
+    def __init__(self):
+        gpio.setmode(gpio.BCM)
+        self.step_pin = 13 #13
+        self.dir_pin = 17   #1
+
+        gpio.setup(self.step_pin,gpio.OUT)
+        gpio.setup(self.dir_pin,gpio.OUT)
+
+    def one_full_turn(self):
+        delay = 500 #500
+        gpio.output(self.dir_pin, gpio.HIGH)
+        for x in range(200):
+            gpio.output(self.step_pin, gpio.HIGH)
+            time.sleep(delay/1000)
+            gpio.outputt(self.step_pin, gpio.LOW)
+            time.sleep(delay/1000)
+
+
+if __name__=="__main__":
+    mot = Motor()
+    mot.one_full_turn()
+
