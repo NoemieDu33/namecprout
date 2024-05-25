b7dac4c739b55ff07c9862c2b6555e2ae7f3c025
Faraday <lindois.jeremy@gmail.com>
2024-05-25 15:32:43 +0200

    fefe

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ gpio.setup(27, gpio.OUT) # avant gauche
 gpio.setup(22, gpio.OUT) # arriere gauche
 gpio.setup(23, gpio.OUT) # avant droit
 gpio.setup(24, gpio.OUT) # arriere droit    
-gpio.output(22, gpio.HIGH)
+gpio.output(22, gpio.LOW)
 gpio.output(24, gpio.HIGH)
