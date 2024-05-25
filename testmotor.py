16796ee5a524f83c0a54cbdaa9a3d12b67bce8ad
NoemieDu33 <hatenasp2@gmail.com>
2024-05-25 02:32:31 +0200

    efzegf

--- testmotor.py
+++ testmotor.py
@@ -1,61 +1,26 @@
-#!/usr/bin/python3             
-# Pour exécuter le prog. en ligne de commande
-from time import sleep         # Importer la bibliothèque de gestion du temps 
-import RPi.GPIO as GPIO        # Importer la bibliothèque de gestion des GPIO
+import sys
+from time import sleep        
+import RPi.GPIO as GPIO        
 
-STEP = 14                      # La commande de pas est reliée au GPIO 14
-DIR = 15                       # La commande de direction est reliée au GPIO 15
-vitesse = 0.0005
+sgn = str(sys.argv[1])
 
-GPIO.setmode(GPIO.BCM)         # Paramétrage de la numérotation des GPIO en mode BCM
-GPIO.setwarnings(False)        # Ne pas tenir comte des alertes
-GPIO.setup(STEP, GPIO.OUT)     # GPIO STEP configuré en sortie
-GPIO.setup(DIR, GPIO.OUT)      # GPIO DIR configuré en sortie
+GPIO.setmode(GPIO.BCM)
 
-# Tourner d'un nombre de pas en CW ou CCW
-def tourne(pas, sens, vitesse):
-    # Sens de rotation
-    if (sens == "CW"):
-        GPIO.output(DIR, GPIO.HIGH)
-    else:
-        GPIO.output(DIR, GPIO.LOW)
-    # Avancer du nombre de pas
-    for x in range(pas):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(vitesse)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(vitesse)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+
+l0 = [27,22,23,24]
+l1 = []
 
 
+for i in range(len(sgn)):
+    if int(sgn[i])==1:
+        l1.append(l0[i])
 
-while True:
-    # On travaille en 1/16 de pas soit 3200 micropas par tour
-    for x in range(3200):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(vitesse)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(vitesse)
+GPIO.output([l1], GPIO.HIGH)
+sleep(1)
+GPIO.output([l0],GPIO.LOW)
+GPIO.cleanup()
 
-    sleep(1)
-
-    GPIO.output(DIR, GPIO.LOW)
-    for x in range(3200):
-        GPIO.output(STEP, GPIO.HIGH)
-        sleep(vitesse)
-        GPIO.output(STEP, GPIO.LOW)
-        sleep(vitesse)
-        
-    sleep(1)	
-    tourne(1600, "CW", vitesse)
-    sleep(0.5)
-    tourne(1600, "CCW", vitesse)
-    sleep(1)	
-    tourne(800, "CW", vitesse)
-    sleep(0.5)
-    tourne(800, "CW", vitesse)
-    sleep(0.5)
-    tourne(800, "CW", vitesse)
-    sleep(0.5)
-    tourne(800, "CW", vitesse)
-    
-    sleep(1)
