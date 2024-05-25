d0aca8c163e121d6daafc661eb7044494b9f2fe2
NoemieDu33 <hatenasp2@gmail.com>
2024-05-25 05:13:57 +0200

    egretzryhshtsrt

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from line import Line
-import time
+import time, RPi.GPIO
 
 li = Line()
 for _ in range(100):
     li.update_sensor()
     print(li.sensors)
+    time.sleep(0.5)
+RPi.GPIO.cleanup()
