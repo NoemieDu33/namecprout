a32f19d82646653560d697d5f3a6fc2e3d506358
Faraday <lindois.jeremy@gmail.com>
2024-05-25 15:28:05 +0200

    efe

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 from motor import Motor
 import time,sys
+import RPi.GPIO as gpio  
 
 mo = Motor()
 mo.send_signal("back")
+gpio.setmode(gpio.BCM)
+gpio.setup(27, gpio.OUT) # avant gauche
+gpio.setup(22, gpio.OUT) # arriere gauche
+gpio.setup(23, gpio.OUT) # avant droit
+gpio.setup(24, gpio.OUT) # arriere droit    
+gpio.output(22, gpio.HIGH)
+gpio.output(24, gpio.HIGH)
