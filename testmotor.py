f2202117f7b4a29cf405f622ef093c01eb384104
NoemieDu33 <hatenasp2@gmail.com>
2024-05-25 02:37:55 +0200

    dtgfyqegyuqeryu

--- testmotor.py
+++ testmotor.py
@@ -17,9 +17,8 @@ l1 = []
 
 for i in range(len(sgn)):
     if int(sgn[i])==1:
-        l1.append(l0[i])
+        GPIO.output(l0[i], GPIO.HIGH)
 
-GPIO.output([l1], GPIO.HIGH)
 sleep(1)
 GPIO.output([l0],GPIO.LOW)
 GPIO.cleanup()
